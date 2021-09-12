82f083229b230c61f5d86df751487c9ca46d1700
Skyler Anderson <askskyler@gmail.com>
2021-09-12 18:14:33 -0500

    Move some json parsing into the python script, neaten Departure Time format, use full alert header

--- app.py
+++ app.py
@@ -6,15 +6,36 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 BASE_ENDPOINT = "https://api-v3.mbta.com/"
-SCHEDULES_ARGS = "schedules?include=route,trip,stop&filter[stop]=place-north"
+SCHEDULES_ARGS = "schedules?include=route,trip,stop&filter[stop]=place-north&sort=departure_time&page[limit]=10"
+ALERTS_ARGS = "alerts?filter[stop]=place-north&page[limit]=5"
 
-def get_routes():
+def get_departures():
     response = requests.get(BASE_ENDPOINT + SCHEDULES_ARGS)
-    return response.json()["data"][:10]
+
+    departures = []
+    for schedule in response.json()["data"]:
+        departure_time = schedule['attributes']['departure_time']
+        departure_time = departure_time[11:16]
+        departures.append({
+            "Line": schedule['relationships']['route']['data']['id'],
+            "DepartureTime": departure_time
+        })
+
+    return departures
+
+def get_alerts():
+    response = requests.get(BASE_ENDPOINT + ALERTS_ARGS)
+
+    alerts = []
+    for alert in response.json()["data"]:
+        alerts.append({
+            "Message": alert['attributes']['header']
+        })
+
+    return alerts
 
 @app.route('/')
 def index():
-    routes = get_routes()
-    for route in routes:
-        print(route)
-    return render_template('index.html', routes=routes)
+    departures = get_departures()
+    alerts = get_alerts()
+    return render_template('index.html', departures=departures, alerts=alerts)
